a236ed0f70f5f60cffd20b030991ff33188107ad
huytdps13400 <kingonwork@gmail.com>
2022-01-07 00:41:43 +0700

    update code

--- routes/main.js
+++ routes/main.js
@@ -9,8 +9,14 @@ router.get("/manage-users", async (req, res) => {
   const User = db.collection("User");
 
   const snapshot = await User.get();
+
   const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
   res.render("../pages/manage-users.ejs", { data: list });
+});
+router.delete("/manage-users-delete/:id", async (req, res) => {
+  let id = req.params.id;
+  await db.collection("User").doc(id).delete();
+  res.send({ ress: true });
 });
 
 router.get("/manage-users2", (req, res) => {
