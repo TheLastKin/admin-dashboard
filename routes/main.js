92528e389165720f284570a4b125a3d865926942
Nhat <nhatnguyenhoang0@gmail.com>
2022-01-07 00:51:45 +0700

    update

--- routes/main.js
+++ routes/main.js
@@ -13,6 +13,22 @@ router.get("/manage-users", async (req, res) => {
   res.render("../pages/manage-users.ejs", { data: list });
 });
 
+router.get("/manage-products", async (req, res) => {
+  const Product = db.collection("products");
+  const snapshot = await Product.get();
+  let data = [];
+  for(let i = 0; i < snapshot.docs.length; i++){
+    await db.collection(`products/${snapshot.docs[i].id}/featureproduct`).get().then(snapshot2 => {
+      data.push(...snapshot2.docs.map(doc => ({id: doc.id, ...doc.data()})));
+    });
+    await db.collection(`products/${snapshot.docs[i].id}/newachives`).get().then(snapshot3 => {
+      data.push(...snapshot3.docs.map(doc => ({id: doc.id, ...doc.data()})))
+    });
+  }
+  console.log(data);
+  res.render("../pages/manage-products.ejs", { data: data });
+})
+
 router.get("/manage-users2", (req, res) => {
   res.render("../pages/tables/basic-table.ejs");
 });
