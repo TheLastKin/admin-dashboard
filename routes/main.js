6e236a1611e6f538c4f631dacaa82e356cb62e33
huytdps13400 <kingonwork@gmail.com>
2022-01-17 23:05:08 +0700

    update code

--- routes/main.js
+++ routes/main.js
@@ -289,9 +289,9 @@ router.get("/statistical", async (req, res) => {
 
   const snapshot = await Order.get();
   const orders = snapshot.docs.map((doc) => {
-    let dateOrder = moment(doc.data().date?._seconds * 1000).format(
-      "DD/MM/YYYY"
-    );
+    let dateOrder = doc.data().date
+      ? moment(doc.data().date._seconds * 1000).format("DD/MM/YYYY")
+      : moment().format("DD/MM/YYYY");
     return {
       id: doc.id,
       dateOrder: dateOrder,
